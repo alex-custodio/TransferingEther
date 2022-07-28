f7b49444ce1f9250b3e4cd42048f298de8afafb4
alexcstd <alex.scustod@gmail.com>
2022-07-28 17:46:24 -0300

    smart contract deploy added

---
+++ README.md
@@ -0,0 +1,25 @@
+# TransferingEther
+Here comes my project to improve my web3j skills, in this repo I will try to make a simple ether transaction
+
+## Tools
++ Java
++ Gradle
++ Web3j 
++ IntelliJ
+
+## How it works: 
+The main code is on src/main/java folder and I called it Transaction.java. The code makes a simple ether transaction (I tested this using Ganache and it helped me a lot). 
++ 1 - I created the Web3j object, that works like a client and it gives us the access to the power of creating a transaction. 
++ 2 - After I made the GAS_PRICE, GAS_LIMIT, RECIPIENT (which contains the public address of who will receive the ether), and PRIVATE_KEY. 
++ 3 - So it is easy to use the private key to create the credentials and with those credentials we can make the core of a transaction.
++ 4 - I think the code is easy to read so if you try to review you will get it.
+
+
+I would never have made it without: [this tutorial](https://www.youtube.com/watch?v=kJ905hVbQ_E&list=PL16WqdAj66SCOdL6XIFbke-XQg2GW_Avg&index=32)
+(In this test I )
+
+
+# Portuguese
+## Como funciona 
+
+// faço dps to com preguiça
--- src/main/java/com/transaction/Transaction.java
+++ src/main/java/com/transaction/Transaction.java
@@ -1,5 +1,6 @@
-package test;
+package com.transaction;
 
+import com.transaction.Human;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -7,6 +8,8 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.RawTransactionManager;
 import org.web3j.tx.TransactionManager;
 import org.web3j.tx.Transfer;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
 
 import java.math.BigDecimal;
@@ -18,8 +21,9 @@ This code makes a simple ether transaction
 public class Transaction {
 
     public final static String PRIVATE_KEY = "";
-    public final static BigInteger GAS_PRICE = BigInteger.valueOf(2000000000L);
-    public final static BigInteger GAS_LIMIT = BigInteger.valueOf(6721975L);
+    public final static BigInteger GAS_PRICE = BigInteger.valueOf(20000L);
+    public final static BigInteger GAS_LIMIT = BigInteger.valueOf(2000000000000000L);
+    public final static ContractGasProvider contractGasProvider = new DefaultGasProvider();
     public final static String RECIPIENT = "";
     public static void main(String[] args) {
         try {
@@ -29,10 +33,12 @@ public class Transaction {
         }
     }
     private Transaction() throws Exception {
-        Web3j web3j = Web3j.build(new HttpService(""));
+        Web3j web3j = Web3j.build(new HttpService("http://127.0.0.1:7545"));
         TransactionManager transactionManager = new RawTransactionManager(web3j, getCredentialsFromPrivateKey());
         Transfer transfer = new Transfer(web3j, transactionManager);
         TransactionReceipt transactionReceipt = transfer.sendFunds(RECIPIENT, BigDecimal.TEN, Convert.Unit.ETHER, GAS_PRICE, GAS_LIMIT).send();
+        String contractAddress = Human.deploy(web3j,getCredentialsFromPrivateKey(), contractGasProvider).send().getContractAddress();
+        System.out.print(contractAddress);
     }
     public static Credentials getCredentialsFromPrivateKey(){
         return Credentials.create(PRIVATE_KEY);
